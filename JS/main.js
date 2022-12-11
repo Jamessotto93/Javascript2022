24e44d5d17fac417c4d57ac61b26c84537545fe5
[Jamessotto93} <[james_santiago93@outlook.com]>
2022-12-11 00:11:36 -0300

    Actualizacion

--- JS/main.js
+++ JS/main.js
@@ -1,75 +1,46 @@
-let index= prompt ("¡Hola, Bienvenido a Refugio Don Torcuato! \n ¿Qué mascota te gustaría adoptar? Ingresá la opción que corresponda \n 1 - Adoptar un perro \n 2 - Adoptar un gato \n 0 - Salir");
+// Crear variables para almacenar el stock, el precio y el total de cada producto
+let stockPantalones = 0;
+let precioPantalones = 100;
+let stockChaquetas = 0;
+let precioChaquetas = 150;
+let stockCamperas = 0;
+let precioCamperas = 200;
+let stockRemeras = 0;
+let precioRemeras = 50;
+let total = 0;
 
-if (index != "0") {
+carritoDeCompras()
 
-    switch (index) {
-        case "1": 
-        let perroAdoptado= prompt("Ellos estan esperando un hogar: \n 1 - Roco - cachorro mediano \n 2 - Pepa - adulta pequeña \n Ingresa el número que le corresponde para adoptarlo o 0 para salir");
-        
-            switch (perroAdoptado){
-                case "1":
-                    alert("Gracias! Nos vamos a comunicar con vos para completar el formulario y terminar la adopción");
-                    let adoptanteRoco=prompt("Por favor indicanos tu nombre");
-                    let mailRoco= prompt("Por favor ingresa tu email");
-                    alert("En minutos te llegará el formulario al mail " + mailRoco);
-                    console.log( "El adoptante es " + adoptanteRoco + " y su email es " + mailRoco);   
-                break;
+function carritoDeCompras() {
+    // Crear un bucle que se ejecute mientras el usuario no ingrese "Fin" para finalizar la compra
+    while (true) {
+      // Pedir al usuario que elija un producto
+      let producto = prompt("Por favor elija un producto: Pantalones, Chaquetas, Camperas, Remeras o Fin para finalizar la compra");
+   // Si el usuario elige Fin, mostrar el total de todos los productos comprados y terminar el proceso de compra
+   if (producto === "Fin") {
+    alert("El total de todos los productos comprados es $" + total);
+    return;
+  }
 
-                case "2":
-                    alert("Gracias! Nos vamos a comunicar con vos para completar el formulario y terminar la adopción");
-                    let adoptantePepa=prompt("Por favor indicanos tu nombre");
-                    let mailPepa= prompt("Por favor ingresa tu email");
-                    alert("En minutos te llegará el formulario al mail " + mailPepa);
-                    console.log( "El adoptante es " + adoptantePepa + " y su email es " + mailPepa);
-                break;
+  // Pedir al usuario que ingrese la cantidad del producto elegido
+  let cantidad = prompt("Por favor ingrese la cantidad del producto elegido:");
 
-                case "0":
-                    alert("Hasta la próxima, te esperamos!");
-                break;
-
-                default:
-                    alert("Opción no válida, por favor vuelva a intentarlo");
-                break;
-            }
-            break;
-
-            case "2":
-                let gatoAdoptado= prompt("Ellos estan esperando un hogar: \n 1- Pelu hembra adulta \n 2 - Tito - macho bebe \n Ingresa el número que le corresponde para adoptarlo o 0 para salir");
-
-                switch (gatoAdoptado){
-                    case "1":
-                        alert("Gracias! Nos vamos a comunicar con vos para completar el formulario y terminar la adopción");
-                        let adoptantePelu=prompt("Por favor indicanos tu nombre");
-                        let mailPelu= prompt("Por favor ingresa tu email");
-                        alert("En minutos te llegará el formulario al mail " + mailPelu);
-                        console.log( "El adoptante es " + adoptantePelu + " y su email es " + mailPelu);
-                        
-                    break;
-
-                    case "2":
-                        alert("Gracias! Nos vamos a comunicar con vos para completar el formulario y terminar la adopción");
-                        let adoptanteTito=prompt("Por favor indicanos tu nombre");
-                        let mailTito= prompt("Por favor ingresa tu email");
-                        alert("En minutos te llegará el formulario al mail " + mailTito);
-                        console.log( "El adoptante es " + adoptantetito + " y su email es " + mailTito);
-            
-                    break;
-
-                    case "0":
-                        alert("Hasta la próxima, te esperamos!");
-                        break;
-
-                    default:
-                        alert("Opción no válida, por favor vuelva a intentarlo");
-                        break;
-                }
-            break;
-
-            default:
-                alert("Opción no válida, por favor vuelva a intentarlo");
-            break;
-        
+  // Determinar qué producto ha elegido el usuario y agregar la cantidad especificada al stock correspondiente
+  switch (producto) {
+    case "Pantalones":
+      stockPantalones += cantidad;
+      total += cantidad * precioPantalones;
+      alert ("Producto agregado al carrito");
+      break;
+    case "Chaquetas":
+      stockChaquetas += cantidad;
+      total += cantidad * precioChaquetas;
+      alert ("Producto agregado al carrito");
+      break;
+    case "Camperas":
+      stockCamperas += cantidad;
+      total += cantidad * precioCamperas;
+      alert ("Producto agregado al carrito");
+    }
+    }
 }
-}
-alert("Hasta la próxima, te esperamos!");
-
