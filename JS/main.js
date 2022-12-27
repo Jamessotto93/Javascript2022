4444c56ac9c08361d35b6a65c0b2833a798d038a
[Jamessotto93} <[james_santiago93@outlook.com]>
2022-12-27 20:29:56 -0300

    segunda entrega

--- JS/main.js
+++ JS/main.js
@@ -1,46 +1,109 @@
-// Crear variables para almacenar el stock, el precio y el total de cada producto
-let stockPantalones = 0;
-let precioPantalones = 100;
-let stockChaquetas = 0;
-let precioChaquetas = 150;
-let stockCamperas = 0;
-let precioCamperas = 200;
-let stockRemeras = 0;
-let precioRemeras = 50;
-let total = 0;
+class Contacto {
+  constructor (nombre, apellido, email, telefono){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.telefono = telefono;
+  }
+}
 
-carritoDeCompras()
+let contactos = [];
+let operando = true;
 
-function carritoDeCompras() {
-    // Crear un bucle que se ejecute mientras el usuario no ingrese "Fin" para finalizar la compra
-    while (true) {
-      // Pedir al usuario que elija un producto
-      let producto = prompt("Por favor elija un producto: Pantalones, Chaquetas, Camperas, Remeras o Fin para finalizar la compra");
-   // Si el usuario elige Fin, mostrar el total de todos los productos comprados y terminar el proceso de compra
-   if (producto === "Fin") {
-    alert("El total de todos los productos comprados es $" + total);
+
+while (operando) {
+  let opcion = Number(prompt(`
+                        Bienvenidos a Agenda.com.ar
+                        "Seleccione una opcion"
+                        1) AGREGAR UN NUEVO CONTACTO
+                        2) BORRAR UN CONTACTO
+                        3) LISTAR TODOS LOS CONTACTOS 
+                        4) BUSCAR UN CONTACTO
+                        
+                        PARA SALIR PRESIONE ENTER O INGRESE "0"`));
+
+  switch (opcion) {
+    case 0:
+      alert(`Se cerró el sistema`);
+      operando = false;
+      break;
+    case 1:
+      let nom = prompt("Ingrese Nombre del Contacto");
+      let ape = prompt("Ingrese Apellido del Contacto");
+      let email = prompt("Ingrese el e-mail del contacto").toLocaleLowerCase();
+      let tel = Number(prompt("Ingrese Telefono del Contacto"));
+      
+      if (typeof nom === 'string' || nom === "" && typeof ape === 'string' || ape === "" && typeof email === 'string' || email === "" && typeof tel == 'number' || tel === "") {
+        contacto = new Contacto(nom, ape, email, tel);
+        agregarContacto();
+        cuentaContactos();
+      } else {
+        alert("controle los Datos Ingresados");
+      }
+      break;
+      case 2:
+         emailContacto = prompt(`Ingrese el mail del Contacto que desea Eliminar`).toLocaleLowerCase();
+        eliminaContacto(emailContacto);   
+        break;
+      case 3:
+         listarContactos();
+        break;
+      case 4:
+        let cadena = prompt("Ingrese el nombre, apellido, correo electrónico o teléfono del contacto que desea encontrar");
+        buscarContactos(cadena);
+        break;
+    default:
+      alert("Seleccione una opción válida");
+  }
+}
+
+
+function agregarContacto(){
+  contactos.push(contacto);
+  console.log(contactos);
+}
+
+function eliminaContacto(mail){
+  const resultado = contactos.find(contacto => contacto.email === mail);
+  if(resultado === undefined){
+    console.log(`No existe ese e-mail en tus Contactos`);
+  }else{
+    console.log(resultado);
+    let indice = contactos.findIndex(contacto => contacto.email === mail);
+    //alert(indice);
+    contactos.splice(indice, 1);
+    cuentaContactos();
+  }
+}
+
+function cuentaContactos(){
+  console.log(`Tu Agenda tiene ${contactos.length} Contactos`);
+}
+
+function listarContactos() {
+  if (contactos.length === 0) {
+    alert('Agenda vacía');
     return;
   }
 
-  // Pedir al usuario que ingrese la cantidad del producto elegido
-  let cantidad = prompt("Por favor ingrese la cantidad del producto elegido:");
-
-  // Determinar qué producto ha elegido el usuario y agregar la cantidad especificada al stock correspondiente
-  switch (producto) {
-    case "Pantalones":
-      stockPantalones += cantidad;
-      total += cantidad * precioPantalones;
-      alert ("Producto agregado al carrito");
-      break;
-    case "Chaquetas":
-      stockChaquetas += cantidad;
-      total += cantidad * precioChaquetas;
-      alert ("Producto agregado al carrito");
-      break;
-    case "Camperas":
-      stockCamperas += cantidad;
-      total += cantidad * precioCamperas;
-      alert ("Producto agregado al carrito");
-    }
-    }
+  for(let contacto of contactos){
+    alert(`Datos de los Contactos nombre: ${contacto.nombre}, Apellido, ${contacto.apellido}, e-mail: ${contacto.email}, telefono ${contacto.telefono}`);
+  }
 }
+listarContactos ()
+
+function buscarContactos(cadena) {
+  let resultados = contactos.filter(contacto => {
+    return contacto.nombre.includes(cadena) || contacto.apellido.includes(cadena) || contacto.email.includes(cadena) || contacto.telefono.toString().includes(cadena);
+  });
+
+  if (resultados.length === 0) {
+    alert('No se han encontrado resultados');
+    return;
+  }
+
+  for (let contacto of resultados) {
+    alert(`Datos de los Contactos nombre: ${contacto.nombre}, Apellido, ${contacto.apellido}, e-mail: ${contacto.email}, telefono ${contacto.telefono}`);
+  }
+}
+buscarContactos ()
