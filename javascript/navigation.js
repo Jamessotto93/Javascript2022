f79813afb6ee504284b0d7fce6ceff48675c60ed
[Jamessotto93} <[james_santiago93@outlook.com]>
2023-02-01 20:37:12 -0300

    proyecto final santiago sottocasa

---
+++ javascript/navigation.js
@@ -0,0 +1,34 @@
+const navToggler = document.getElementById('navToggler');
+const navUl = document.getElementById('navUl');
+const menuBars = document.querySelectorAll('.menuBar');
+const navLinks = document.querySelectorAll('.navLink');
+
+function toggleMenu() {
+    navUl.classList.toggle('hidden');
+
+    menuBars.forEach(bar => {
+        bar.classList.toggle('active');
+    })
+}
+
+navToggler.addEventListener('click', toggleMenu);
+
+navLinks.forEach(link => {
+    link.addEventListener('click', toggleMenu)
+})
+
+navLinks[3].addEventListener('click', (e) => {
+    e.preventDefault();
+    Swal.fire({
+        titleText: '¿Bugs o consultas?',
+        html:
+          `<br><h2>Podés encontrarme acá ↓</h2><br><a href="https://www.linkedin.com/in/santiago-sottocasa-675743b6/" target="_blank"><i class="fa-brands fa-linkedin-in fa-4x" style="color: #00BCE1; filter: drop-shadow(0 0 0.1rem #00BCE1);" alt='linkedIn link'></i></a>`,
+        focusConfirm: false,
+        background: '#051C2C',
+        color: '#fff',
+        confirmButtonText:
+          'De vuelta al sitio',
+        confirmButtonColor: '#00BCE1',
+        customClass:{ confirmButton: 'swalBtnCustom'}
+      })
+})
